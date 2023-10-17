0d27cab2eac071c0f367e361f1ae1e26254fdac8
ttiimmothy <dinosauli2006@gmail.com>
2023-10-17 14:07:25 -0400

    Fix: change crudrepository to jparepository

--- src/main/java/ca/openricecan/MainApplication.java
+++ src/main/java/ca/openricecan/MainApplication.java
@@ -1,18 +1,16 @@
 package ca.openricecan;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 @SpringBootApplication
 @RestController
 public class MainApplication {
-  @RequestMapping("/")
+  @RequestMapping
 	public String home() {
 		return "Welcome to Open Rice Canada";
 	}
@@ -21,13 +19,13 @@ public class MainApplication {
         SpringApplication.run(MainApplication.class, args);
     }
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:3000");
-            }
-        };
-    }
+//  @Bean
+//  public WebMvcConfigure corsConfigure() {
+//    return new WebMvcConfigure() {
+//        @Override
+//        public void addCorsMappings(CorsRegistry registry) {
+//            registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+//        }
+//    };
+//  }
 }
--- src/main/java/ca/openricecan/repository/restaurant/RestaurantRepository.java
+++ src/main/java/ca/openricecan/repository/restaurant/RestaurantRepository.java
@@ -1,11 +1,12 @@
 package ca.openricecan.repository.restaurant;
 
 import ca.openricecan.data.entity.restaurant.RestaurantEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface RestaurantRepository extends CrudRepository<RestaurantEntity, UUID> {
+public interface RestaurantRepository extends JpaRepository<RestaurantEntity, UUID> {
 }
